c833b8a0167739df6a9f0025c643ca3f4bfeaafc
bakaa_1 <158278526+moegeeks@users.noreply.github.com>
2024-07-05 06:38:17 +0900

    delete

--- src/commandBase.ts
+++
@@ -1,7 +0,0 @@
-import { Message } from 'discord.js';
-
-export interface CommandBase {
-    name: string,
-    execute: (message: Message) => void,
-    wantExecute: (contents: string) => boolean,
-}
--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import { MoeClient } from './moeclient';
-import * as botinfo from '../botinfo.json';
-
-const client = new MoeClient();
-
-client.start(botinfo.token);
