c841326e3601787bd302f1f531520f8290fe25a4
bakaa_1 <158278526+moegeeks@users.noreply.github.com>
2024-07-06 04:05:54 +0900

    Refactor

--- Tools/Logger.cs
+++ Tools/Logger.cs
@@ -1,27 +1,27 @@
 namespace MoeBot.Tools {
     internal static class Logger {
-        static private void Log(string message, ConsoleColor colour) {
+        private static void Log(string message, ConsoleColor colour) {
             Console.ForegroundColor = colour;
             Console.WriteLine(message);
             Console.ResetColor();
         }
 
-        static internal void Log(string message) {
+        internal static void Log(string message) {
             DateTime dateTime = DateTime.Now;
             Log($"{dateTime.ToString("HH:mm:ss")} Log         {message}", ConsoleColor.White);
         }
 
-        static internal void DiscordCommand(string message) {
+        internal static void DiscordCommand(string message) {
             DateTime dateTime = DateTime.Now;
             Log($"{dateTime.ToString("HH:mm:ss")} Command     {message}", ConsoleColor.Blue);
         }
 
-        static internal void Error(string message) {
+        internal static void Error(string message) {
             DateTime dateTime = DateTime.Now;
             Log($"{dateTime.ToString("HH:mm:ss")} Error       {message}", ConsoleColor.Red);
         }
 
-        static internal void Warning(string message) {
+        internal static void Warning(string message) {
             DateTime dateTime = DateTime.Now;
             Log($"{dateTime.ToString("HH:mm:ss")} Warning     {message}", ConsoleColor.Yellow);
         }
